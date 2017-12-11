1303e4b2b37b3de6bd60dff35ea9fe40e46d4e7c
Jaymes Garland <JaymesGarland@gmail.com>
2017-12-11 16:57:16 -0500

    Deleted seed file and some comments that do not need to be in the program.

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ const bodyParser = require("body-parser");
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt-nodejs');
-//const users = require('./data/users');
+const users = require('./data/users');
+const cards = require('data/cards');
 const app = express();
 const exphbs = require("express-handlebars");
 const handlebars = require("handlebars");
-
-const users = require("./userDatabaseUserCollection");
-const cards = require("./cardCollection");
 
 app.use(bodyParser.json());
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -102,42 +100,3 @@ app.listen(27017, () => {
     console.log("We've now got a server!");
     console.log("Your routes will be running on http://localhost:27017/");
 });
-
-
-// Dependencies:
-// 'express' allows MVC implementation on server side code
-// 'body-parser' populates req.body with the value of the POST command
-// 'path' allows us to get other files from the project directory
-// 'express-handlebars' allows us to construct different views for different pages
-// const express = require('express'); 
-// const bodyParser = require('body-parser');
-// const path = require('path');
-// var exphbs = require('express-handlebars');
-
-// Initialize App
-// let app = express();
-
-// Initialize Routes
-// let routes = require('./routes/index');
-// let users = require('./routes/users');
-
-// View Engine
-// app.set('views', path.join(__dirname, 'views'));
-// app.engine('handlebars', exphbs({defaultLayout:'layout'}));
-// app.set('view engine', 'handlebars');
-
-// BodyParser Middleware
-// app.use(bodyParser.json());
-
-// Set Static Folder (for bootstrap)
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// Add routes to express application
-// app.use('/', routes);
-// app.use('/users', users);
-
-// Start Server
-// app.listen(27017, () => {
-//     console.log("We've now got a server!");
-//     console.log("Your routes will be running on http://localhost:27017/");
-// });
