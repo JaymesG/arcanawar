112276fc1b4ff69b5cd14f11d6ac046327c37de1
Jaymes Garland <JaymesGarland@gmail.com>
2017-12-13 12:52:51 -0500

    Deleted commented out functions in cards.js

--- data/cards.js
+++ data/cards.js
@@ -9,49 +9,6 @@ let exportedMethods = {
         const cardArray = await cardCollection.find({}).toArray();
         return cardArray;
     }
-
-    // async getCardsById(id) {
-    //     if(!id) throw "No ID provided."
-    //     const cardCollection = await cards();
-    //     const card = await cardCollection.findOne({
-    //         _id: id
-    //     });
-
-    //     if (!card) throw "Card not found";
-    //     return card;
-    // },
-
-    // async addCard(title, desc, type, value) {
-    //     if (typeof title !== "string") throw "Title must be of type string.";
-    //     if (typeof desc !== "string") throw "Description must be of type string.";
-    //     if (typeof type !== "string") throw "Type must be of type string.";
-    //     if (typeof value !== "number") throw "Value must be of type number.";
-
-    //     const cardCollection = await cards();
-
-    //     const newCard = {
-    //         _id: uuid.v4(),
-    //         title: title,
-    //         desc: desc,
-    //         type: type,
-    //         value: value
-    //     };
-
-    //     const newInsertInformation = await cardCollection.insertOne(newCard);
-    //     const newId = newInsertInformation.insertedId;
-    //     return await this.getCardById(newId);
-    // },
-
-    // async removeCard(id) {
-    //     if(!id) throw "No ID provided."
-    //     const cardCollection = await card();
-    //     const deletionInfo = await cardCollection.removeOne({
-    //         _id: id
-    //     });
-    //     if (deletionInfo.deletedCount === 0) {
-    //         throw `Could not delete card with id of ${id}`;
-    //     }
-    // }
 };
 
 module.exports = exportedMethods;
