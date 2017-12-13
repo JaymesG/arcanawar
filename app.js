d629b308eab99caa0e802ffba8c7221ea0be5dc2
Jaymes Garland <JaymesGarland@gmail.com>
2017-12-13 11:17:02 -0500

    Changes made from lastnight

--- app.js
+++ app.js
@@ -1,9 +1,88 @@
+// Hashing algorithm makes login slow.
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt-nodejs');
 const users = require('./data/users');
+const cards = require('./data/cards');
+//const cards = [{
+            "title": "Glare",
+            "desc": "It seems looks really can kill.",
+            "type": "Attack",
+            "value": 2
+        },
+        {
+            "title": "Final Presentation",
+            "desc": "It's like that dream, where you're standing in your underwear.",
+            "type": "Attack",
+            "value": 10
+        },
+        {
+            "title": "Black Cat",
+            "desc": "Some think black cats are unlucky, this one isn't.",
+            "type": "Defense",
+            "value": 5
+        },
+        {
+            "title": "Glowing Butterfly",
+            "desc": "Oooh, something shiny!",
+            "type": "Defense",
+            "value": 1
+        },
+        {
+            "title": "Vlad",
+            "desc": "He vants to suck your blood.",
+            "type": "Attack",
+            "value": 7
+        },
+        {
+            "title": "Lucky Goldfish",
+            "desc": "Heart of gold, this fish is worth its weight in gold.",
+            "type": "Defense",
+            "value": 6
+        },
+        {
+            "title": "Green Thumb",
+            "desc": "They said she could grow anything, so she grows vines that choke the life out of her enemy.",
+            "type": "Attack",
+            "value": 5
+        },
+        {
+            "title": "Luna Sol",
+            "desc": "Sunrise, sunset, you don't want to get this celestial being upset.",
+            "type": "Attack",
+            "value": 3
+        }, {
+            "title": "Beyonce",
+            "desc": "She doesn't really need a description.",
+            "type": "Attack",
+            "value": 10
+        },
+        {
+            "title": "Water Bender",
+            "desc": "Can heal you with the power of water.",
+            "type": "Defense",
+            "value": 10
+        },
+        {
+            "title": "Gray wolf",
+            "desc": "attack",
+            "type": "Yes, this wolf is really wicked. Spine - tingling howl is all which is required",
+            "value": 7
+        },
+        {
+            "title": "Sea cucumber",
+            "type": "defense",
+            "desc": "This isn't an ordinary Sea cucumber. It expels out its internal organs, which is sticky and very toxic",
+            "value": 8
+        }
+    ]
+
+
+
+const _ = require('underscore');
 const app = express();
 const exphbs = require("express-handlebars");
 const handlebars = require("handlebars");
@@ -65,8 +144,24 @@ function protectPrivate(req, res, next) {
     }
 }
 
+const cardList = cards.getAllCards();
+//const cardString = JSON.stringify(cardList);
+
+const userPoints = 20;
+const aiPoints = 30;
+// const userCards = _.sample(allcards, 5);
+// const aiCards = _.sample(allcards, 5);
+
 app.get("/privategame", protectPrivate, function (req, res) {
     res.render('pages/privategame', {
+        aiHP: userPoints,
+        userHP: aiPoints,
+        cards: card
+    });
+});
+
+app.get("/profile", protectPrivate, function (req, res) {
+    res.render('pages/profile', {
         user: req.user
     });
 });
