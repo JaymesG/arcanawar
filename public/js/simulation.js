decc68a2a273ac9a9d45ef265a93dc6b576e5ea0
Jaymes Garland <JaymesGarland@gmail.com>
2017-12-13 20:27:29 -0500

    he

--- public/js/simulation.js
+++ public/js/simulation.js
@@ -2,8 +2,69 @@ var list = document.getElementById('savedInputs');
 var ls = document.getElementById('aiHP');
 var ls2 = document.getElementById('userHP');
 var uCards = document.getElementById('cardsContainer');
-var userCards= [];
-var aiCards = [];
+// var userCards = [];
+// var aiCards = [];
+
+var userCards = [{
+    "title": "Glare",
+    "desc": "It seems looks really can kill.",
+    "type": "Attack",
+    "value": 2
+},
+{
+    "title": "Final Presentation",
+    "desc": "It's like that dream, where you're standing in your underwear.",
+    "type": "Attack",
+    "value": 10
+},
+{
+    "title": "Black Cat",
+    "desc": "Some think black cats are unlucky, this one isn't.",
+    "type": "Defense",
+    "value": 5
+},
+{
+    "title": "Glowing Butterfly",
+    "desc": "Oooh, something shiny!",
+    "type": "Defense",
+    "value": 1
+},
+{
+    "title": "Vlad",
+    "desc": "He vants to suck your blood.",
+    "type": "Attack",
+    "value": 7
+}]
+
+aiCards = [{
+    "title": "Lucky Goldfish",
+    "desc": "Heart of gold, this fish is worth its weight in gold.",
+    "type": "Defense",
+    "value": 6
+},
+{
+    "title": "Green Thumb",
+    "desc": "They said she could grow anything, so she grows vines that choke the life out of her enemy.",
+    "type": "Attack",
+    "value": 5
+},
+{
+    "title": "Luna Sol",
+    "desc": "Sunrise, sunset, you don't want to get this celestial being upset.",
+    "type": "Attack",
+    "value": 3
+}, {
+    "title": "Beyonce",
+    "desc": "She doesn't really need a description.",
+    "type": "Attack",
+    "value": 10
+},
+{
+    "title": "Water Bender",
+    "desc": "Can heal you with the power of water.",
+    "type": "Defense",
+    "value": 10
+}]
 
 var aiHP = 20;
 var userHP = 20;
@@ -68,20 +129,21 @@ var userHP = 20;
                 errorContainer.classList.add("hidden");
 
                 const userInputValue = userInputElement.value;
+                console.log(userInputValue);
 
-                const simpleInput = cardMethods.simplify(userInputValue);
-                const result = cardMethods.check(simpleInput);
+                // const simpleInput = cardMethods.simplify(userInputValue);
+                // const result = cardMethods.check(simpleInput);
                 userHP--;
                 aiHP -= 5;
 
-                var entry = document.createElement('li');
-                if(result) {
-                    entry.classList.add('is-palindrome');
-                } else {
-                    entry.classList.add('not-palindrome');
-                }
-                entry.appendChild(document.createTextNode(simpleInput));
-                list.appendChild(entry);
+                // var entry = document.createElement('li');
+                // if(result) {
+                //     entry.classList.add('is-palindrome');
+                // } else {
+                //     entry.classList.add('not-palindrome');
+                // }
+                // entry.appendChild(document.createTextNode(simpleInput));
+                // list.appendChild(entry);
                 ls.innerHTML = aiHP;
                 ls2.innerHTML = userHP;
 
